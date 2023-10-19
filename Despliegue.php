1c765eed2e05233e2449f8dfcbf32d109a20b71d
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2023-10-19 08:24:50 -0500

    Update Despliegue.php

--- Despliegue.php
+++ Despliegue.php
@@ -3,5 +3,6 @@
 include('Funciones.php');
 
 echo consulta()."<br>"; //Se invoca la funcion con un espacio entre linea
+echo Calculo()."<br>"; //Se invoca la funcion con un espacio entre linea
 echo Aprender(); //Se invoca la funcion
 ?>
