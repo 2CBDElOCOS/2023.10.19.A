e42a9359900b59b319ba4922539764b6e46afd26
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2023-10-19 11:40:45 -0500

    Update Despliegue.php

--- Despliegue.php
+++ Despliegue.php
@@ -1,9 +1,14 @@
 <?php
 //Se incluye el archivo funciones
 include('Funciones.php');
-
+/*
 echo consulta()."<br>"; //Se invoca la funcion con un espacio entre linea
 echo Calculo_1()."<br>"; //Se invoca la funcion con un espacio entre linea
 echo Calculo_2()."<br>"; //Se invoca la funcion
-echo Count_Users(); //Se invoca la funcion
+echo Count_Users()."<br>"; //Se invoca la funcion
+echo insertar_datos('5','Ricardo','200','2023-10-19','2')."<br>"; //Se invoca la funcion
+echo Delete_User();
+echo insertar_sitio('https://www.youtube.com/watch?v=oujzyZfQF94&list=RDEMvWDXtsNXCdhBqBgBUWxJug&index=2&ab_channel=DeiV','1')."<br>";
+*/
+echo mostrar_sitio('1');
 ?>
