28091d1f177867ef582d634e91cf87801780f89f
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2023-10-19 07:27:57 -0500

    Update Funciones.php

--- Funciones.php
+++ Funciones.php
@@ -8,3 +8,15 @@ function consulta(){
     return $Salida;     //Retorna la salida con dicho resultado
 
 }
+
+function Aprender(){
+
+    $Salida=0;         //Se iniciliza la variable
+
+    $Salida=2*2;       //Calcula el area de un triangulo
+
+    return $Salida;    //Retorna la salida con dicho resultado
+
+}
+
+?>
