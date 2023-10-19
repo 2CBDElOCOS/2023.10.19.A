d6e6802f4a11b73f191d45dc15760a6e3e6059c9
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2023-10-19 12:35:36 -0500

    Update Despliegue.php

--- Despliegue.php
+++ Despliegue.php
@@ -7,9 +7,9 @@ echo Calculo_1()."<br>"; //Se invoca la funcion con un espacio entre linea
 echo Calculo_2()."<br>"; //Se invoca la funcion
 echo Count_Users()."<br>"; //Se invoca la funcion
 echo insertar_datos('5','Ricardo','200','2023-10-19','2')."<br>"; //Se invoca la funcion
-echo Delete_User();
-echo insertar_sitio('https://www.youtube.com/watch?v=oujzyZfQF94&list=RDEMvWDXtsNXCdhBqBgBUWxJug&index=2&ab_channel=DeiV','1')."<br>";
-*/
-echo mostrar_sitio('1');
+echo Delete_User();*/
+echo insertar_sitio('https://www.sena.edu.co/es-co/Paginas/default.aspx','1')."<br>";
+//echo mostrar_sitio('1');
+echo mostrar_sitio1('1','Capacitate');
 
 ?>
