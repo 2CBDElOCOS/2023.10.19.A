2d989b6ec1616502afdab2884783974db158aadb
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2023-10-19 08:24:18 -0500

    Update Funciones.php

--- Funciones.php
+++ Funciones.php
@@ -6,12 +6,35 @@ function consulta(){
 
     $sql="Select 2+1";  //Consulta SQL
     $sql.=" as suma";    //Consulta SQL
-    $resultado = $conexion->query($sql);//Ejecuta la consulta
+    $resultado = $conexion->query($sql);    //Ejecuta la consulta
 
     //Recorre el recordset
     While($Fila=mysqli_fetch_assoc($resultado)){
         $Salida+=$Fila['suma'];    //Inch o acumula
     }
+
+    $conexion->close(); //Se finaliza la conexion
+
+    return $Salida;     //Retorna la salida con dicho resultado
+
+}
+
+function Calculo(){
+
+    $Salida=0;          //Se iniciliza la variable
+    $conexion = mysqli_connect('localhost', 'root', 'root', 'practica_03');//Se establece la conexion a la base de datos
+
+    $sql="Select 10 as n1,";  //Consulta SQL
+    $sql.=" 20 as n2";    //Consulta SQL
+    $resultado = $conexion->query($sql);    //Ejecuta la consulta
+
+    //Recorre el recordset
+    While($Fila=mysqli_fetch_assoc($resultado)){
+        $Salida+=$Fila['n1'];      //Inch o acumula
+        $Salida+=$Fila['n2'];     //Inch o acumula
+    }
+
+    $conexion->close(); //Se finaliza la conexion
 
     return $Salida;     //Retorna la salida con dicho resultado
 
